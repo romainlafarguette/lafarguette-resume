3449c016d1993d588346965d1ec7e1811d9671de
Romain Lafarguette <romain.lafarguette@gmail.com>
2021-07-19 09:24:45 -0400

    Countries update

--- auto/lafarguette_resume.el
+++ auto/lafarguette_resume.el
@@ -4,13 +4,13 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("cv" "usegeometry" "10pt" "a4paper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("fontenc" "T1") ("geometry" "top=1.5cm" "bottom=1cm" "left=2.2cm" "right=2.2cm" "paper=a4paper" "textwidth=180mm")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("geometry" "top=1.5cm" "bottom=1cm" "left=2cm" "right=1.5cm" "paper=a4paper" "textwidth=200mm")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
