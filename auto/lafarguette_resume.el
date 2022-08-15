ad2b2baa89f40d4f7db0c6afd80b6f8b5f6e6540
Romain Lafarguette <A13258Q@adia.ae>
2022-08-15 18:22:17 +0400

    Add ADIA to the CB

--- auto/lafarguette_resume.el
+++ auto/lafarguette_resume.el
@@ -5,12 +5,13 @@
                      '(("cv" "usegeometry" "10pt" "a4paper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("geometry" "top=1.8cm" "bottom=1.2cm" "left=2cm" "right=1.5cm" "paper=a4paper" "textwidth=200mm")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -34,8 +35,8 @@
     "twodots"
     "midreduce")
    (LaTeX-add-environments
-    "short"
-    "normal"
-    "long"))
+    "short-itemize"
+    "normal-itemize"
+    "long-itemize"))
  :latex)
 
